1f3cc0442f4677c1b0425dcff310f524b20ae143
Chris Schmich <schmch@gmail.com>
2019-12-18 16:38:34 -0800

    Add Netlify deploy script.

---
+++ deploy/deploy.sh
@@ -0,0 +1,5 @@
+set -euf -o pipefail
+
+cd $(dirname $0)
+cp -R ../web web
+curl -H "Content-Type: application/vnd.bitballoon.v1.raw" "https://api.netlify.com/api/v1/sites/$netlify_site_id/files/index.html?access_token=$netlify_access_token" -o web/spoilers.json
