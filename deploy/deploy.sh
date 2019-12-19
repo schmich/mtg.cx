c8a64c1a16b1cda929cc6bf259746fcef08dcf59
Chris Schmich <schmch@gmail.com>
2019-12-18 16:44:30 -0800

    Fix path.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@ set -euf -o pipefail
 
 cd $(dirname $0)
 cp -R ../web web
-curl -H "Content-Type: application/vnd.bitballoon.v1.raw" "https://api.netlify.com/api/v1/sites/$netlify_site_id/files/index.html?access_token=$netlify_access_token" -o web/spoilers.json
+curl -H "Content-Type: application/vnd.bitballoon.v1.raw" "https://api.netlify.com/api/v1/sites/$netlify_site_id/files/spoilers.json?access_token=$netlify_access_token" -o web/spoilers.json
