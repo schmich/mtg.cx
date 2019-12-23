fa348c1ca8f503fea6de82c4574c6858d0035218
Chris Schmich <schmch@gmail.com>
2019-12-23 15:10:16 -0800

    Update deploy script to run relative to the web folder.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,3 @@
 set -euf -o pipefail
 
-cd $(dirname $0)
-cp -R ../web web
-curl "https://mtg-cx.netlify.com/spoilers.json" -o web/spoilers.json
+curl "https://mtg-cx.netlify.com/spoilers.json" -o spoilers.json
